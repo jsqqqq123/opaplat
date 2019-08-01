8b881e39b3b2904dd552d8e8653eebaa61f7008a
jsqqqq123 <jsqqqq123@gmail.com>
2019-08-01 13:41:14 +0800

    add oppplat urls and view to dispaly hello world

---
+++ opplat/opplat/views.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+#
+# @Version : 1.0
+# @Time    : 2019/08/01 13:35
+# @Author  : qq
+# @File    : views
+#
+# django views
+
+from django.http import HttpResponse, HttpResponseRedirect
+
+
+def index(request):
+    HttpResponse("hello world")
+
+
