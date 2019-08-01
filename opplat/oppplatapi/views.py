57e6ff5d35fc4bc8641d4db344a1f19aafe8ab6e
jsqqqq123 <jsqqqq123@gmail.com>
2019-08-01 15:25:50 +0800

    test1

--- opplat/oppplatapi/views.py
+++ opplat/oppplatapi/views.py
@@ -18,7 +18,27 @@ jwt_get_username_from_payload = api_settings.JWT_PAYLOAD_GET_USERNAME_HANDLER
 
 class MyTest(APIView):
     def get(self, request, *args, **kwargs):
-        res = {"username": "jsqqqq", "password": "hello123"}
+        res = {
+                "172.16.16.16": [{
+                "url": "www.baidu.com",
+                "err": False,
+                "message": "",
+                "area": "华东-上海节点",
+                "time": -1564581541.246279
+                }, {
+                "url": "www.sohu.com",
+                "err": True,
+                "message": "www.sohu.com: this domain is hijacked",
+                "area": "华东-上海节点",
+                "time": -1564581541.4576719
+                }, {
+                "url": "www.sina.com",
+                "err": True,
+                "message": "www.sina.com: this domain is hijacked",
+                "area": "华东-上海节点",
+                "time": -1564581541.6912775
+                }]
+            }
         ser = TestSerializer(instance=res)
         # jres = json.dumps(res, ensure_ascii=False)
 
